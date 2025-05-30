04a6621f5d1da03c403a7a10d4593e890e6cf2a3
Yaan768 <yaan7682019@gmail.com>
2025-05-29 21:11:06 -0500

    Actuliza el ejercicio: Agregar validación para longitudes positivas

--- ejercicio13.cpp
+++ ejercicio13.cpp
@@ -1,13 +1,20 @@
-#include <iostream>//formar un triangulo
+#include <iostream> // formar un triangulo
 using namespace std;
+
 int main() {
-    float a, b, c;//a,b,c: longitudes a ingresar 
+    float a, b, c; // a, b, c: longitudes a ingresar
+
     cout << "Ingrese las tres longitudes: ";
     cin >> a >> b >> c;
-    if (a + b > c and a + c > b and b + c > a) {
+
+    // Validación: no se permiten ceros ni números negativos
+    if (a <= 0 || b <= 0 || c <= 0) {
+        cout << "Las longitudes deben ser mayores que cero." << endl;
+    }
+    else if (a + b > c && a + c > b && b + c > a) {
         cout << "Se puede formar un triangulo." << endl;
     } 
-	else {
+    else {
         cout << "No se puede formar un triangulo." << endl;
     }
 }
