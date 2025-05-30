0c29c6448353a677ef979bdf36986e333d8f71ac
Yaan768 <yaan7682019@gmail.com>
2025-05-29 21:37:37 -0500

    Actualiza el ejercicio 14: colocar validacion de entrada

--- ejercicio14.cpp
+++ ejercicio14.cpp
@@ -1,18 +1,25 @@
-#include <iostream>//
+#include <iostream> // Relacionar número con vocal
 using namespace std;
+
 int main() {
     int num;
-    //Entrada
+
+    // Entrada
     cout << "Ingrese un numero del 1 al 5: ";
     cin >> num;
-    //Proceso
-    switch (num) {
-        case 1: cout << "La vocal es A"; break;//Salida
-        case 2: cout << "La vocal es E"; break;//Salida
-        case 3: cout << "La vocal es I"; break;//Salida
-        case 4: cout << "La vocal es O"; break;//Salida
-        case 5: cout << "La vocal es U"; break;//Salida
-        default: cout << "Numero fuera de rango (1-5)";//Salida
+
+    // Validación
+    if (num < 1 || num > 5) {
+        cout << "Error: el numero debe estar entre 1 y 5." << endl;
+    } else {
+        // Proceso
+        switch (num) {
+            case 1: cout << "La vocal es A." << endl; break;
+            case 2: cout << "La vocal es E." << endl; break;
+            case 3: cout << "La vocal es I." << endl; break;
+            case 4: cout << "La vocal es O." << endl; break;
+            case 5: cout << "La vocal es U." << endl; break;
+        }
     }
 }
 
