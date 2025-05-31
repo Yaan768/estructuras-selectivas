eeff37db1e92ac63c8417ba791b184fd8ad31f37
Yaan768 <yaan7682019@gmail.com>
2025-05-30 22:11:24 -0500

    Mejora del ejercicio 16: Mejor interfaz y agregar validación

--- ejercicio16.cpp
+++ ejercicio16.cpp
@@ -1,26 +1,35 @@
-#include <iostream>//Numero credito segun la tarjeta 
+#include <iostream> //Número crédito según la tarjeta 
 using namespace std;
+
 int main() {
     int tip;
     float cred, nuevCred;
-    //Entrada 
+
+    // Entrada 
     cout << "Ingrese el tipo de tarjeta (1, 2, 3 u otro): ";
     cin >> tip;
+
+    // Validación de crédito
     cout << "Ingrese el credito actual: ";
     cin >> cred;
-    //Proceso
+
+    if (cred < 0) {
+        cout << "El credito no puede ser negativo." << endl;
+        return 0;
+    }
+
+    // Proceso
     if (tip == 1) {
         nuevCred = cred * 1.25;
-    } 
-	else if (tip == 2) {
+    } else if (tip == 2) {
         nuevCred = cred * 1.35;
-    } 
-	else if (tip == 3) {
+    } else if (tip == 3) {
         nuevCred = cred * 1.40;
-    } 
-	else {
+    } else {
         nuevCred = cred * 1.50;
     }
-    //Salida
-    cout << "El nuevo credito aumentado es: " << nuevCred;
+
+    // Salida
+    cout << "El nuevo credito aumentado es: S/. " << nuevCred << endl;
 }
+
