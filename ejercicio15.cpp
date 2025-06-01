782a5831ce15fc25ec4785bea6cff1ae31986d08
Yaan768 <yaan7682019@gmail.com>
2025-05-31 23:03:09 -0500

    Ejercicio 15: Mejor interfaz y mejora en el código

--- ejercicio15.cpp
+++ ejercicio15.cpp
@@ -1,21 +1,34 @@
 #include <iostream> // Determinar el día de la semana
 using namespace std;
-
 int main() {
     int dia;
+    // Interfaz
+    cout << "==============================\n";
+    cout << "     DIAS DE LA SEMANA ??     \n";
+    cout << "==============================\n";
+    cout << "1 = Lunes\n";
+    cout << "2 = Martes\n";
+    cout << "3 = Miercoles\n";
+    cout << "4 = Jueves\n";
+    cout << "5 = Viernes\n";
+    cout << "6 = Sabado\n";
+    cout << "7 = Domingo\n";
+    cout << "------------------------------\n";
     // Entrada
-    cout << "Ingrese un número del 0 al 6 (0 = Domingo, 6 = Sábado): ";
+    cout << "Ingrese un numero del 1 al 7: ";
     cin >> dia;
-    // Proceso y salida
+    // Proceso 
+    cout << "Resultado: ";
     switch (dia) {
-        case 0: cout << "Domingo"; break;
-        case 1: cout << "Lunes"; break;
-        case 2: cout << "Martes"; break;
-        case 3: cout << "Miércoles"; break;
-        case 4: cout << "Jueves"; break;
-        case 5: cout << "Viernes"; break;
-        case 6: cout << "Sábado"; break;
-        default: cout << "Número inválido. Debe estar entre 0 y 6."; break;
+        case 1: cout << "Hoy es Lunes "; break;//Salida
+        case 2: cout << "Hoy es Martes "; break;//Salida
+        case 3: cout << "Hoy es Miercoles "; break;//Salida
+        case 4: cout << "Hoy es Jueves "; break;//Salida
+        case 5: cout << "Hoy es Viernes "; break;//Salida
+        case 6: cout << "Hoy es Sabado "; break;//Salida
+        case 7: cout << "Hoy es Domingo "; break;//Salida
+        default: cout << "Numero invalido. Debe estar entre 1 y 7."; break;//Salida
     }
+    cout << "\n==============================\n";
 }
 
