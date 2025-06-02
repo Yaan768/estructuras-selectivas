ad772144da346fb28e7b57a1cfb9611335176297
Yaan768 <yaan7682019@gmail.com>
2025-06-01 21:38:40 -0500

    Mejorar el ejercicio 12: Mejor interfaz y validadcion

--- ejercicio12.cpp
+++ ejercicio12.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <limits> // Para limpiar el buffer en caso de error
 using namespace std;
+
 int main() {
     float x, g;
-    cout << "Ingrese el valor de x: ";
-    cin >> x;
 
+    // Entrada con validación
+    cout << "=====================================\n";
+    cout << "     CALCULAR LA FUNCION g(x)       \n";
+    cout << "=====================================\n";
+    cout << "Ingrese el valor de x: ";
+
+    while (!(cin >> x)) {
+        cout << "Entrada invalida. Por favor, ingrese un numero valido: ";
+        cin.clear(); // Limpia el estado de error
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpia el buffer
+    }
+
+    // Proceso: cálculo de g(x)
     if (x > 8) {
         g = 2 * x;
     } else if (x > 0) {
@@ -13,6 +26,9 @@ int main() {
         g = x * x;
     }
 
+    // Salida
+    cout << "-------------------------------------\n";
     cout << "El valor de g(x) es: " << g << endl;
+    cout << "=====================================\n";
 }
 
