70244981a6b84c68156f7e82602e47b3a3eb5c17
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-03 22:30:23 -0500

    Agregar ejercicio 9 de estructuras selectivas

---
+++ Ejercicio 9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, mayor;
+    cout << "Ingrese tres numeros: ";
+    cin >> a >> b >> c;
+    mayor = a;
+    if (b > mayor) mayor = b;
+	if (c > mayor) mayor = c;
+    cout << "Mayor: " << mayor << endl;
+    return 0;
+}
